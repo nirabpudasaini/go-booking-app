4bdfeb0c17ea656002f08395068dcf13dc719cfd
Nirab Pudasaini <developer.nirab@gmail.com>
2024-07-24 20:11:54 +0300

    constant are read only

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	var confrenceName = "Go confrence"
+	const confrenceTickets = 50
+	var remainingTickets = 50
 
 	fmt.Println("Welcome to", confrenceName, "booking application")
+	fmt.Println("Total tickets:", confrenceTickets, "and", "Remaining Tickets:", remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
